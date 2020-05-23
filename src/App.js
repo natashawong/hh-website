00a57efae5ee439f3c27cffbc0724cbdb0fe7893
Natasha Wong <>
2020-05-23 16:12:49 +0800

    - Added react-router, finished routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import AboutUs from './Pages/AboutUs';
+import Home from './Pages/Home';
+import Happenings from './Pages/Subpages/Happenings';
+import MissionVision from './Pages/Subpages/MissionVision';
+import Read from './Pages/Subpages/Read';
+
+// TO DO: add a NoMatch page for errors
 
 function App() {
   return (
-    <div className="App">
-      <AboutUs/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/aboutus'>
+          <AboutUs/>
+        </Route>
+
+        <Route path='/happenings'>
+          <Happenings/>
+         </Route>
+
+        <Route path='/missionvission'>
+          <MissionVision/>
+        </Route>
+
+        <Route path='/read'>
+          <Read/>
+        </Route>
+
+        <Route path='/'>
+          <Home/>
+        </Route>
+
+      </Switch>
+
+    </Router>
   );
 }
 

a4d484c81a40e846e411502efd512217817c658b
natashawong <natasha.wong10@gmail.com>
2020-07-14 17:29:34 +0800

    Changed images from local to imgur bc they weren't loading in prod, might have to change more

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,32 +6,20 @@ import arrowDown from '../Assets/arrowDown.png';
 import './Home.css';
 import ContentGrid from '../Components/contentGrid';
 
-// Images
-import staff from '../Assets/temp.jpeg';
-import skyrug from '../Assets/skyrug.png';
-import hawaiilove from '../Assets/hawaiilove.jpeg';
-import qanc from '../Assets/qandc.png';
-
-import ppllaunch from '../Assets/ppllaunch.jpeg';
-import vol2 from '../Assets/vol2.jpeg';
-import whitelogo from '../Assets/whitelogo.png';
-import veritas from '../Assets/veritas.png';
-
-
 export default class Home extends Component {
     componentDidMount() {
         window.scrollTo(0,0);
     }
     render() {
         const rowOne = [
-            {image: staff, title: "meet our staff", link: '/aboutus'},
-            {image: skyrug, title: "what do we do when the rug is pulled out from underneath us?", link: '/read'},
-            {image: hawaiilove, title: "love in the time of coronavirus", link: '/read'},
-            {image: qanc, title: "queerness and christianity", link: '/read'},
-            {image: ppllaunch, title: "launch symposium", link: '/happenings'},
-            {image: vol2, title: "read volume 2", link: '/archives'},
-            {image: whitelogo, title: "join hearhere", link: '/contact', blackText: true},
-            {image: veritas, title: "veritas forum", externalLink: 'http://www.veritas.org/', blackText: true},
+            {image: "https://i.imgur.com/482Jxh9.jpg", title: "meet our staff", link: '/aboutus'},
+            {image: "https://i.imgur.com/DCVSZhv.jpg", title: "what do we do when the rug is pulled out from underneath us?", link: '/read'},
+            {image: "https://i.imgur.com/EvsaQXs.jpg", title: "love in the time of coronavirus", link: '/read'},
+            {image: "https://i.imgur.com/UfmdehE.png", title: "queerness and christianity", link: '/read'},
+            {image: "https://i.imgur.com/mMSEWHd.jpg", title: "launch symposium", link: '/happenings'},
+            {image: "https://i.imgur.com/sEqJQ1p.jpg", title: "read volume 2", link: '/archives'},
+            {image: "https://i.imgur.com/BFLl0yO.png", title: "join hearhere", link: '/contact', blackText: true},
+            {image: "https://i.imgur.com/GMUIaoD.png", title: "veritas forum", externalLink: 'http://www.veritas.org/', blackText: true},
         ]
 
         return (
